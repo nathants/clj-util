bad2c5d250c8e3a43444685b2a7f1c191598a87a
nathants <me@nathants.com>
2015-06-16 00:27:29 -0700

    for-map

--- src/util.clj
+++ src/util.clj
@@ -2,9 +2,17 @@
   (:require [clojure.string :as s]))
 
 
-(defmacro str-format [& strs]
+(defmacro str-format
+  [& strs]
   (let [string (apply str strs)
         pattern #"\#\{([^\}]+)\}"
         parts (s/split string pattern)
         symbols (->> string (re-seq pattern) (map second) (map symbol) vec)]
     `(apply str (interleave ~parts ~(conj symbols "")))))
+
+
+(defmacro for-map
+  [bindings pair]
+  `(->> (for ~bindings ~pair)
+        (apply concat)
+        (apply hash-map)))
